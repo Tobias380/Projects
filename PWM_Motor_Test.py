fcae47ff4c4de6b019bc7054e459e2d34b998bf3
Tobias380 <tobias_38@hotmail.com>
2013-11-06 16:58:44 +0000

    Written Code

--- PWM_Motor_Test.py
+++ PWM_Motor_Test.py
@@ -1,1 +1,34 @@
 import time 
+import RPi.GPIO as gpio
+from Adafruit_PWM_Servo_Driver import PWM
+
+gpio.setmode (gpio.Board)
+pwm = PWM(0x40, debug=True)
+gpio.setup (7, gpio.out)
+gpio.setup (12, gpio.out)
+
+def Spin1 ():
+  gpio.output(7, True)
+  gpio.output(12, False)
+  pwm.setPWMFreq(60)
+  pwm.setPWM (0,0,2000) 
+  time.sleep(2)
+  
+def Spin2 ():
+  gpio.output(7, True)
+  gpio.output(12, False)
+  pwm.setPWMFreq(60)
+  pwm.setPWM (0,0,2000) 
+  time.sleep(2)
+  
+try:
+  while (True):
+    print 'Spin 1'
+    Spin1 ()
+    print 'Spin2'
+    Spin2 ()
+except KeyboardInterrupt :
+  pwm.setPWM (0,0,0) 
+  gpio.cleanup ()
+    
+  
